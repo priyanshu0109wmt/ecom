d5e35231cabf539a92b9350872e39a9cfa070370
priyanshu0109wmt <priyanshuth9@gmail.com>
2026-03-25 17:59:37 +0530

    fix: bind server to 0.0.0.0

--- backend/server.js
+++ backend/server.js
@@ -214,6 +214,6 @@ app.get("/orders/:user_id", (req, res) => {
 // ================= SERVER =================
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port", PORT);
 });
