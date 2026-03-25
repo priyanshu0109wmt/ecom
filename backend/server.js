0cb6d307116578042747f9f4a919151f28de92b4
priyanshu0109wmt <priyanshuth9@gmail.com>
2026-03-25 19:13:12 +0530

    final fix: port fallback + binding

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,8 @@ app.get("/health", (req, res) => {
   res.send("OK");
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0",() => {
   console.log("Server running on port", PORT);
 });
